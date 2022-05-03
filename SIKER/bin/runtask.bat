f05a086182fcb43c2bfef218b02824f3c0ea1181
csgellert <csapodi.gellert@gmail.com>
2022-05-03 19:14:18 +0200

    modify files

--- SIKER/bin/runtask.bat
+++ SIKER/bin/runtask.bat
@@ -1,1 +1,1 @@
-"C:\Users\barnabaspiri\AppData\Local\Programs\Julia-1.7.2\bin\julia" --color=yes --depwarn=no --project=@. -q -- "%~dp0..\bootstrap.jl" -r %*
+julia --color=yes --depwarn=no --project=@. -q -- "%~dp0..\bootstrap.jl" -r %*
