6ad395a567f0758d6b298f960df1671ed490c3a3
BARNABAS-PC\barnabaspiri <barnabaspiri@gmail.com>
2022-05-04 02:00:28 +0200

    added: canvas for drawing with form inputs

--- SIKER/public/01_test_script.js
+++ SIKER/public/01_test_script.js
@@ -1,4 +1,11 @@
+console.log("01_test_script.js file is connected!")
+
 submit_button.addEventListener("click", getKPOI); // Button click event listener
+
+// ------------------------------------------------------------------------------- // 
+
+var canvas = document.getElementById("myCanvas"); // Defining canvas object
+var context = canvas.getContext("2d"); // Defining the context of the canvas
 
 
 function getKPOI() { // Function to trigger when submit button is pressed
@@ -11,4 +18,11 @@ function getKPOI() { // Function to trigger when submit button is pressed
 
   console.log("x-coordinate: ", Xcoord)
   console.log("y-coordinate: ", Ycoord)
+
+  context.clearRect(0, 0, canvas.width, canvas.height) // Clear canvas before drawing
+  context.beginPath(); // Begin drawing in canvas
+  context.arc(Xcoord, Ycoord, 10, 0, 2*Math.PI); // Create an arc at Xcoord, Ycoord with R = 10
+  context.closePath();
+  context.fill();
+
 }
