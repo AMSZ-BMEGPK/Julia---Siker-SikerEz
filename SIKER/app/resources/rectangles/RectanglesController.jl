7ebe26a759fe444ebd1e4da452d23e1b89cce843
csgellert <csapodi.gellert@gmail.com>
2022-05-25 16:01:12 +0200

    communication established

---
+++ SIKER/app/resources/rectangles/RectanglesController.jl
@@ -0,0 +1,25 @@
+module RectanglesController
+  using Genie.Renderer.Html
+
+  struct Rect
+    width::Int32
+    height::Int32
+  end
+
+  const x1 = 10
+  const y1 = 100
+
+  function calculate_points()
+    "
+    <h1>Drawing Rect</h1>
+    <div style='position:absolute; top: $(x1); right: $(y1); width: 200px; height: 100px; border: 3px solid #73AD21;'>
+      contents
+    </div>
+    "
+  end
+
+  function rectangle_view(a,b)
+    html(:rectangles, :rectangleview, x1= x1, y1=y1,w=a,h=b)
+  end
+
+end
--- SIKER/routes.jl
+++ SIKER/routes.jl
@@ -1,5 +1,23 @@
 using Genie.Router
+using  RectanglesController
+using Genie.Requests
 
+global belso = 3
 route("/") do
   serve_static_file("welcome.html")
 end
+
+route("/rw") do 
+  RectanglesController.rectangle_view(200,200)
+end
+route("/rw", method = POST) do 
+  a = postpayload(:fname,"100")
+  b = postpayload(:lname,"200")
+  RectanglesController.rectangle_view(a,b)
+end
+
+route("/ask_for_input") do 
+  serve_static_file("asking_for_input.html")
+end
+
+
